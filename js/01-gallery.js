8059ab70b5e860ea5006f7a34bcf8215658527a3
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-02-07 11:02:10 +0200

    fix mistakes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const galleryContainer = document.querySelector('.gallery');
+const galleryContainer = document.querySelector(".gallery");
 let indexOfImage = -1;
 
 const arrOfGalleryItems = galleryItems.map(galleryItem => createGallaryItem(galleryItem));
 galleryContainer.append(...arrOfGalleryItems);
 
 function createGallaryItem(galleryItem) {
-  const itemContainer = document.createElement('div');
-  itemContainer.classList.add('gallery__item');
-  const itemLink = document.createElement('a');
-  itemLink.classList.add('gallery__link');
+  const itemContainer = document.createElement("div");
+  itemContainer.classList.add("gallery__item");
+  const itemLink = document.createElement("a");
+  itemLink.classList.add("gallery__link");
   itemLink.href = `${galleryItem.original}`;
-  const imageItem = document.createElement('img');
-  imageItem.classList.add('gallery__image');
+  const imageItem = document.createElement("img");
+  imageItem.classList.add("gallery__image");
   imageItem.src = `${galleryItem.preview}`;
   imageItem.alt = `${galleryItem.description}`;
   imageItem.dataset.source = `${galleryItem.original}`;
@@ -23,15 +23,15 @@ function createGallaryItem(galleryItem) {
   return itemContainer;
 }
 
-galleryContainer.addEventListener('click', event => {
+galleryContainer.addEventListener("click", event => {
   event.preventDefault();
   const itemRef = event.target;
-  if (itemRef.nodeName !== 'IMG') {
+  if (itemRef.nodeName !== "IMG") {
     return;
   }
 
-  const content = document.createElement('div');
-  const lightBoxImage = document.createElement('img');
+  const content = document.createElement("div");
+  const lightBoxImage = document.createElement("img");
   lightBoxImage.src = itemRef.dataset.source;
   lightBoxImage.alt = itemRef.alt;
   lightBoxImage.dataset.indexoflargeimage = itemRef.dataset.indexofimage;
@@ -45,15 +45,15 @@ galleryContainer.addEventListener('click', event => {
   basicLightInstans.show();
 
   function addWindowListener() {
-    window.addEventListener('keydown', changeLargeImageIndex);
-    window.addEventListener('keydown', pressEscBtn);
+    window.addEventListener("keydown", changeLargeImageIndex);
+    window.addEventListener("keydown", pressEscBtn);
   }
   function removeWindowListener() {
-    window.addEventListener('keydown', changeLargeImageIndex);
-    window.removeEventListener('keydown', pressEscBtn);
+    window.removeEventListener("keydown", changeLargeImageIndex);
+    window.removeEventListener("keydown", pressEscBtn);
   }
   function pressEscBtn(event) {
-    if (event.code === 'Escape') {
+    if (event.code === "Escape") {
       basicLightInstans.close();
     }
   }
@@ -61,7 +61,7 @@ galleryContainer.addEventListener('click', event => {
   function changeLargeImageIndex(event) {
     const currentImgIndex = Number(lightBoxImage.dataset.indexoflargeimage);
 
-    if (event.code === 'ArrowLeft') {
+    if (event.code === "ArrowLeft") {
       const prevImgIindex = currentImgIndex - 1;
 
       if (prevImgIindex >= 0) {
@@ -71,7 +71,7 @@ galleryContainer.addEventListener('click', event => {
       }
     }
 
-    if (event.code === 'ArrowRight') {
+    if (event.code === "ArrowRight") {
       const nextImgIndex = currentImgIndex + 1;
 
       if (nextImgIndex < galleryItems.length) {
